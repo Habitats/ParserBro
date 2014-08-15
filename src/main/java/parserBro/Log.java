e6d9add97a9075b57ebe8b59fe7597e05ac17fa0
Patrick <mail@habitats.no>
2014-08-15 22:49:56 +0200

    formatting

--- src/main/java/parserBro/Log.java
+++ src/main/java/parserBro/Log.java
@@ -2,19 +2,19 @@ package parserBro;
 
 public abstract class Log {
 
-	public static void i(String string) {
-		logMessage("INFO: " + string);
-	}
+  public static void i(String string) {
+    logMessage("INFO: " + string);
+  }
 
-	public static void w(String string) {
-		logMessage("WARNING: " + string);
-	}
+  public static void w(String string) {
+    logMessage("WARNING: " + string);
+  }
 
-	public static void e(String string) {
-		logMessage("ERROR:" + string);
-	}
+  public static void e(String string) {
+    logMessage("ERROR:" + string);
+  }
 
-	protected static void logMessage(String string) {
-		System.out.println(string);
-	}
+  protected static void logMessage(String string) {
+    System.out.println(string);
+  }
 }
